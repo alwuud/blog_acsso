56e0060691186e735fe5d5afef869e60aeffdfc2
alwud <usac.daniel@gmail.com>
2023-07-17 21:13:54 -0600

    Se agrego plataforma administracion para blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css/');
     eleventyConfig.addPassthroughCopy('./src/images');
-    eleventyConfig.addPassthroughCopy('./src/js')
+    eleventyConfig.addPassthroughCopy('./src/js');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
         return {
             dir: {
